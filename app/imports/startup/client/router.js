2c2d791eae24702bc452cb61fbf4f55b8ad31c88
dankoo <danieldk@hawaii.edu>
2016-10-25 00:11:50 -1000

    part 1 of mockup

--- app/imports/startup/client/router.js
+++ app/imports/startup/client/router.js
@@ -8,24 +8,53 @@ FlowRouter.route('/', {
   },
 });
 
-FlowRouter.route('/list', {
-  name: 'List_Stuff_Page',
+
+FlowRouter.route('/user_profile', {
+  name: 'User_Profile_Page',
   action() {
-    BlazeLayout.render('App_Body', { main: 'List_Stuff_Page' });
+    BlazeLayout.render('App_Body', { main: 'User_Profile_Page' });
   },
 });
 
-FlowRouter.route('/add', {
-  name: 'Add_Stuff_Page',
+FlowRouter.route('/user_home', {
+  name: 'User_Home_Page',
   action() {
-    BlazeLayout.render('App_Body', { main: 'Add_Stuff_Page' });
+    BlazeLayout.render('App_Body', { main: 'User_Home_Page' });
   },
 });
 
-FlowRouter.route('/stuff/:_id', {
-  name: 'Edit_Stuff_Page',
+FlowRouter.route('/events', {
+  name: 'Events_Page',
   action() {
-    BlazeLayout.render('App_Body', { main: 'Edit_Stuff_Page' });
+    BlazeLayout.render('App_Body', { main: 'Events_Page' });
+  },
+});
+
+FlowRouter.route('/list_item', {
+  name: 'List_Item_Page',
+  action() {
+    BlazeLayout.render('App_Body', { main: 'List_Item_Page' });
+  },
+});
+
+FlowRouter.route('/create_item', {
+  name: 'Create_Item_Page',
+  action() {
+    BlazeLayout.render('App_Body', { main: 'Create_Item_Page' });
+  },
+});
+
+FlowRouter.route('/promoter', {
+  name: 'Promoters_Page',
+  action() {
+    BlazeLayout.render('App_Body', { main: 'Promoters_Page' });
+  },
+});
+
+FlowRouter.route('/user_profile', {
+  name: 'User_Profile_Page',
+  action() {
+    BlazeLayout.render('App_Body', { main: 'User_Profile_Page' });
   },
 });
 
--- app/imports/ui/pages/index.js
+++ app/imports/ui/pages/index.js
@@ -1,6 +1,21 @@
 import './add-stuff-page.html';
 import './add-stuff-page.js';
 
+import './User_Profile_Page.html';
+import './User_Profile_Page.js';
+
+import './User_Home_Page.html';
+import './User_Home_Page.js';
+
+import './Events_Page.html';
+import './Events_Page.js';
+
+import './List_Item_Page.html';
+import './List_Item_Page.js';
+
+import './Create_Item.html';
+import './Create_Item.js';
+
 import './edit-stuff-page.html';
 import './edit-stuff-page.js';
 
